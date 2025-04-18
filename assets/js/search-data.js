bdadb723a7f0fcf51f8d628e3f794661926d8d6c
david0530 <31782193+david0530@users.noreply.github.com>
2025-04-18 07:16:32 +0000

    Deploying to gh-pages from @ david0530/david0530.github.io@e4f5cac045755d6fe136b7a951acc445e921d4f9 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -398,11 +398,11 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
+            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+          description: "",
+          section: "News",},{id: "news-i-gave-a-poster-presentation-in-ieee-isbi-2025-in-houston-tx",
+          title: 'I gave a poster presentation in IEEE ISBI 2025 in Houston, TX',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
